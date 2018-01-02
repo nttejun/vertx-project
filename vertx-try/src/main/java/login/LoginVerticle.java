30bbc5b7f632f73bf498488a3ca1a485222502d9
Wonjun Jeong <nttejun@gmail.com>
2018-01-02 15:17:31 +0900

    Use router and parameter value

--- vertx-try/src/main/java/login/LoginVerticle.java
+++ vertx-try/src/main/java/login/LoginVerticle.java
@@ -12,68 +12,36 @@ public class LoginVerticle extends AbstractVerticle {
 		Vertx vertx = Vertx.vertx();
 		vertx.deployVerticle(new LoginVerticle());
 	}
-	
+
 	@Override
 	public void start(Future<Void> fut) {
-	 // Create a router object.
-	 Router router = Router.router(vertx);
 
-	 // Bind "/" to our hello message - so we are still compatible.
-	 router.route("/").handler(routingContext -> {
-		 HttpServerResponse response = routingContext.response();
-	   response
-	       .putHeader("content-type", "text/html")
-	       .end("<h1>Hello from my first Vert.x 3 application</h1>");
-	 });
+		// Create a router object.
+		Router router = Router.router(vertx);
 
-	 router.route("/login").handler(routingContext -> {
-		 HttpServerRequest request = routingContext.request();
-		 HttpServerResponse response = routingContext.response();
-		 System.out.println("로그인");
-		 System.out.println(request.getParam("id"));
-		   response
-		       .putHeader("content-type", "text/html")
-		       .end("<form id=/'login'/>"
-		       		+ "<input type = /'text'/>"
-		       		+ "</form>");
-		 request.response().end();
-	 });
-	 
-	 // success url
-	 // Bind url test
-	 router.route("/login").handler(routingContext -> {
-		 HttpServerRequest request = routingContext.request();
-		 HttpServerResponse response = routingContext.response();
-		 System.out.println("로그인");
-		   response
-		       .putHeader("content-type", "text/html")
-		       .end("<h1>/login</h1>");
-		 request.response().end();
-	 });
-	 
-	 // error parameter
-	 // Bind login
-	 router.route("/login/:_id").handler(routingContext -> {
-		 HttpServerRequest request = routingContext.request();
-		 System.out.println("로그인");
-		 System.out.println(request.getParam("_id"));
-		 request.response().end();
-	 });
-	 // Create the HTTP server and pass the "accept" method to the request handler.
-	 vertx
-	     .createHttpServer()
-	     .requestHandler(router::accept)
-	     .listen(
-	         // Retrieve the port from the configuration,
-	         // default to 8080.
-	         config().getInteger("http.port", 8083),
-	         result -> {
-	           if (result.succeeded()) {
-	             fut.complete();
-	           } else {
-	             fut.fail(result.cause());
-	           }
-	         }
-	     );
+		// Bind "/" to our hello message - so we are still compatible.
+		router.route("/").handler(routingContext -> {
+			HttpServerResponse response = routingContext.response();
+			response.putHeader("content-type", "text/html").end("<h1>Hello from my first Vert.x 3 application</h1>");
+		});
+
+		router.route("/login/:id").handler(routingContext -> {
+			HttpServerRequest request = routingContext.request();
+			System.out.println("로그인");
+			System.out.println(request.getParam("id"));
+			request.response().end();
+		});
+		
+		// Create the HTTP server and pass the "accept" method to the request handler.
+		vertx.createHttpServer().requestHandler(router::accept).listen(
+				// Retrieve the port from the configuration,
+				// default to 8080.
+				config().getInteger("http.port", 8083), result -> {
+					if (result.succeeded()) {
+						fut.complete();
+					} else {
+						fut.fail(result.cause());
+					}
+				});
 	}
 }
